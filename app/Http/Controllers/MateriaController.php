d53fbe6b165984c09212b75b4a0bea5a28c3e371
maxi <maxikairuz22@hotmail.com>
2017-11-19 17:39:50 -0300

    algo

--- app/Http/Controllers/MateriaController.php
+++ app/Http/Controllers/MateriaController.php
@@ -6,13 +6,5 @@ use Illuminate\Http\Request;
 use App\materia;
 class MateriaController extends Controller
 {
-     public function index($id)
-	
-    {	
-        
-    	$fecha_materia = Materia::where('nombre', $id)->get();
-    	return view('admin.finales_materia.index')->with(compact('materias'));
-    	
-
-    }
+   dd 'hola';
 }
