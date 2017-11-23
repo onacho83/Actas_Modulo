c113af525e382b14d2decdab1eff3cf5f0342834
onacho83 <onacho83@gmail.com>
2017-11-23 19:38:48 -0300

    Revert "encabezado2"
    This reverts commit dc61f5d445174f8c17dea796893ac387f10fb427.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="{{ url('/admin/fecha_Finales') }}">Ifts 12 - Actas - Fecha Finales</a>
+                <a class="navbar-brand" href="{{ url('/admin/fecha_Finales') }}">Ifts 12 - Fecha Finales</a>
             </div>
 
             <div class="collapse navbar-collapse" id="navigation-example">
